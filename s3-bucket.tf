1d5835b5118a052ce034ef3cc05e32c9e023ea0b
Palaniappan <palani.sm@gmail.com>
2022-08-14 13:47:11 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,10 @@ module "s3_bucket" {
 #   source = "terraform-aws-modules/s3-bucket/aws"
   source  = "app.terraform.io/TFE-CHIP-palani/s3-bucket/aws"
   version = "2.8.0"
-  
+
   bucket = "my-s3-bucket"
   acl    = "private"
-  bucket_prefix=var.prefix
+  
   versioning = {
     enabled = true
   }
